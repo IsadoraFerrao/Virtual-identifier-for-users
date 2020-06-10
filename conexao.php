4f52d99667a72f98cb54c5c51de41a2107e24c15
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-06-10 17:58:25 -0300

    conect

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+$con=mysqli_connect("fdb25.awardspace.net","3448101_vid","Byp9D6cX4hA[p({x","3448101_vid");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+?>
